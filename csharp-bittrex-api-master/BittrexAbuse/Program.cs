147bca5244ed2fe1fd9f2e73cc7f3491dd6944b9
Konstantin <rediskadota@gmail.com>
2017-12-08 10:58:03 +0200

    get middle

--- csharp-bittrex-api-master/BittrexAbuse/Program.cs
+++ csharp-bittrex-api-master/BittrexAbuse/Program.cs
@@ -9,9 +9,23 @@ namespace BittrexAbuse
 {
     class Program
     {
+        static Exchange ex = new Exchange();
+        static ExchangeContext context = new ExchangeContext();
+
         static void Main(string[] args)
         {
-            
+            context.ApiKey = "8ef631a422f743eabcbefc288a9b20d0";
+            context.Secret = "419b15331ef64491b76477522bd79770";
+            context.Simulate = false;
+            context.QuoteCurrency = "BTC";
+
+            ex.Initialise(context);
+
+            var tmp = ex.GetMarketSummary("ZEC");
+
+            var c = (tmp.High + tmp.Low) / 2;
+
+            Console.WriteLine(c);
         }
 
 
