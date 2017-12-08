88d3dd6ab7c435d3d3e8c820c5582e09183e75df
Konstantin <rediskadota@gmail.com>
2017-12-08 16:19:59 +0200

    merge

--- csharp-bittrex-api-master/BittrexAbuse/Program.cs
+++ csharp-bittrex-api-master/BittrexAbuse/Program.cs
@@ -10,9 +10,36 @@ namespace BittrexAbuse
 {
     class Program
     {
+        static Exchange e = new Exchange();
+        const string apiKey = "8ef631a422f743eabcbefc288a9b20d0";
+        const string apiSecret = "419b15331ef64491b76477522bd79770";
+        static decimal? purchase;
+        static decimal oldLast;
+        static decimal currentLast;
         static void Main(string[] args)
         {
 
+            ExchangeContext context = new ExchangeContext();
+            context.ApiKey = apiKey;
+            context.Secret = apiSecret;
+            context.QuoteCurrency = "USDT";
+            e.Initialise(context);
+
+
+            //Exchange ex = new Exchange();
+            //ExchangeContext context2 = new ExchangeContext();
+            //context.ApiKey = "8ef631a422f743eabcbefc288a9b20d0";
+            //context.Secret = "419b15331ef64491b76477522bd79770";
+            //context2.Simulate = true;
+            //context2.QuoteCurrency = "USDT";
+
+            //ex.Initialise(context2);
+
+            //var tmp = ex.GetMarketSummary("ZEC");
+
+            //var mid = (tmp.High + tmp.Low) / 2;
+            //var last = tmp.Last;
+            //Console.WriteLine(last);
         }
         static void CheckLast()
         {
@@ -22,7 +49,7 @@ namespace BittrexAbuse
         static void BuyOrder()
         {
             var tmp = e.GetMarketSummary("ZEC");
-            if (tmp.Last<(tmp.High * 0.97m) && tmp.Last<oldLast)
+            if (tmp.Last < (tmp.High * 0.97m) && tmp.Last < oldLast)
             {
                 e.PlaceBuyOrder("ZEC", 0.000000001m, tmp.Last);
                 purchase = tmp.Last;
